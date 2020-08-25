fb2e74986e4d2f92715ded70f5f88796e1227d1c
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-25 02:23:36 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,6 +3,8 @@ class Owner
   attr_accessor :say_species
   attr_reader :species, :name, :say_species
 
+  @@all =[]
+
   def initialize(name, species = "human", say_species = "I am a human.")
     @name = name
     @species = species
