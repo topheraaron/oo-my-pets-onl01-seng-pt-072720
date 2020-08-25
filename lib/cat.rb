7f742ca96e5715b083a15dae226d46c6ac2473b4
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-25 03:27:40 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -5,10 +5,10 @@ class Cat
 
   @@all =[]
 
-  def initialize(name, owner, mood = "nervous")
+  def initialize(name, owner)
     @name = name
     @owner = owner
-    @mood = mood
+    @mood = "nervous"
     @@all << self
   end
 
