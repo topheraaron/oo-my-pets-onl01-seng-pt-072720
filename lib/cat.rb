cec611c596dab8616e7a13e7602a3a60587d8238
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-25 02:26:37 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,8 +3,9 @@ class Cat
   attr_accessor :owner, :mood
   attr_reader :name
 
-  def initialize(name, owner)
+  def initialize(name, owner, mood = "nervous")
     @name = name
     @owner = owner
+    @mood = mood
   end
 end
